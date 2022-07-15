467438881f0abd84b0b35cc16e2e9275b8100850
Coolpic783 <58189796+coolpic783@users.noreply.github.com>
2022-07-15 13:27:33 -0500

    It's alive!
    all the project files are here now!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    public float speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+}
