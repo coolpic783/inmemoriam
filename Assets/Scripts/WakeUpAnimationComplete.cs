0f56a6a91ec2a9f50efdd65a7f079fba8e83f098
Coolpic783 <58189796+coolpic783@users.noreply.github.com>
2022-07-27 21:25:28 -0500

    More progress on the opening cutscene
    fade out is in, just need the fade in and destruction of the player dummy (plus dialogue)

--- Assets/Scripts/WakeUpAnimationComplete.cs
+++ Assets/Scripts/WakeUpAnimationComplete.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class WakeUpAnimationComplete : StateMachineBehaviour
 {
+    public PrologueEventManager PEM;
+    public GameObject UnFade;
+
+    void Start()
+    {
+        PEM = GameObject.FindGameObjectWithTag("Manager").GetComponent<PrologueEventManager>();
+    }
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+   override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        PEM.FadeBlackOutSquare();
+        Instantiate(UnFade);
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
