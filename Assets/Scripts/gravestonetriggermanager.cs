c1c7a4e002e1517d8ec24708b4a3f7816c023110
Kevin Randles <58189796+coolpic783@users.noreply.github.com>
2022-09-24 19:22:12 -0500

    Update gravestonetriggermanager.cs

--- Assets/Scripts/gravestonetriggermanager.cs
+++ Assets/Scripts/gravestonetriggermanager.cs
@@ -7,12 +7,17 @@ public class gravestonetriggermanager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        public GameObject gravestone;
+        public GameObject playerKneel;
+        public GameObject player;
         
+        BoxCollider2d gravebc = gravestone.GetComponent<BoxCollider2d>();
+        BoxCollider2d playerbc = player.GetComponent<BoxCollider2d>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(playerbc)
     {
+        Debug.Log("gravestone cutscene triggered.")
         
     }
 }
