c66c2d814cc1e6043ad2ce15cffa06eac98d665d
Coolpic783 <58189796+coolpic783@users.noreply.github.com>
2022-11-30 16:21:57 -0600

    Gravestone is programmable now
    All that's left to do for the prologue is to program it and then figure out how to implement a dialogue system

--- Assets/Scripts/gravestonetriggermanager.cs
+++ Assets/Scripts/gravestonetriggermanager.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class gravestonetriggermanager : MonoBehaviour
 {
 
-    public static GameObject gravestone;
-    public static GameObject playerKneel;
-    public static GameObject player;
+    public GameObject gravestone;
+    public GameObject playerKneel;
+    public GameObject player;
 
-    BoxCollider2D gravebc = gravestone.GetComponent<BoxCollider2D>();
-    BoxCollider2D playerbc = player.GetComponent<BoxCollider2D>();
-
+    void Start()
+    {
+        BoxCollider2D gravebc = gravestone.GetComponent<BoxCollider2D>();
+        BoxCollider2D playerbc = player.GetComponent<BoxCollider2D>();
+    }
     void OnTriggerEnter2D(BoxCollider2D playerbc)
-{
-    Debug.Log("gravestone cutscene triggered.");
+    {
 
-}
+
+        Debug.Log("gravestone cutscene triggered.");
+    }
+
 }
