80817c54c9dd0b4aeb30cc7f18eabc65c5eab418
Coolpic783 <58189796+coolpic783@users.noreply.github.com>
2023-06-01 21:05:14 -0500

    Framework in place, needs more work
    honestly i'm just pretending to know what i'm doing, send help

--- Assets/Scripts/gravestonetriggermanager.cs
+++ Assets/Scripts/gravestonetriggermanager.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class gravestonetriggermanager : MonoBehaviour
 {
-
+    public Dialogue dialogue;
     public GameObject gravestone;
     public GameObject playerKneel;
     public GameObject player;
+    public bool CutsceneNotTriggered = true;
 
     void Start()
     {
         BoxCollider2D gravebc = gravestone.GetComponent<BoxCollider2D>();
         BoxCollider2D playerbc = player.GetComponent<BoxCollider2D>();
     }
-    void OnTriggerEnter2D(BoxCollider2D playerbc)
+
+    public void TriggerDialogue()
     {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
 
+    void OnTriggerStay2D(BoxCollider2D playerbc)
+    {
+        if (CutsceneNotTriggered)
+        {
+            if (Input.GetKeyDown("E"))
+            {
+                Debug.Log("gravestone cutscene triggered.");
 
-        Debug.Log("gravestone cutscene triggered.");
+                TriggerDialogue();
+                CutsceneNotTriggered = false;
+            }
+        }
     }
 
 }
