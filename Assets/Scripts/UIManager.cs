2e6ebbc0842c3a6181fa7a8a74330e9befb4c840
Sayaka Randles <58189796+coolpic783@users.noreply.github.com>
2023-08-10 11:02:09 -0500

    Update UIManager.cs

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using VIDE_Data;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+
+    public GameObject container_NPC;
+    public GameObject container_PLAYER;
+    public Text text_NPC;
+    public Text[] text_Choices;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,18 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (!VD.isActive)
+            {
+                Begin();
+            }
+        }
+    }
+
+    void Begin()
+    {
+    
     }
 }
